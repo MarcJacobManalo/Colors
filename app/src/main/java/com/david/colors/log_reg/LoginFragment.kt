3df1237de024c3b23989a839c02f320420ebfbcd
Jason David <jasondavid678123@gmail.com>
2020-02-06 14:08:24 +0800

    Apply post method

--- app/src/main/java/com/david/colors/log_reg/LoginFragment.kt
+++ app/src/main/java/com/david/colors/log_reg/LoginFragment.kt
@@ -85,7 +85,7 @@ class LoginFragment : Fragment(),View.OnClickListener {
                         d("onSuccessResponse::",responseCode.toString())}
 
                 },{
-                    d("onFailResponse::",it.message.toString()) }))
+                    d("onFailResponsee::",it.message.toString()) }))
 
 //        retrofitInit.map { t: Response<LoginDataModels> ->
 //            val codeSuccess = t.code()
